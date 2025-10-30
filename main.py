d98a4916852bd8e2fa2b5f18854832a249edb028
SPraveeen <praveenji8498@gmail.com>
2025-10-30 11:41:29 +0530

    update//get_book_by_id//delete

--- main.py
+++ main.py
@@ -9,6 +9,28 @@ app=FastAPI()
 def get_all_books(db:Session=Depends(get_db)):
     return services.get_books(db)
 
+@app.get('/books/{id}',response_model=schemas.Book)
+def get_book_by_id(id:int,db:Session=Depends(get_db)):
+    book_queryset=services.get_book_by_idd(db,id)
+    if book_queryset:
+        return book_queryset
+    else:
+        return HTTPException(status_code=404,detail='Invalid book id is provided')
+
 @app.post('/books/',response_model=schemas.Book)
 def create_new_book(book:schemas.BookCreate,db:Session=Depends(get_db)):
     return services.create_book(db,book)
+
+@app.put('/books/{id}',response_model=schemas.Book)
+def update_book(book:schemas.BookCreate,id:int,db:Session=Depends(get_db)):
+    db_update=services.update_book(db,book,id)
+    if not db_update:
+        raise HTTPException(status_code=404,detail="Book Not found")
+    return db_update
+
+@app.delete('/books/{id}',response_model=schemas.Book)
+def delete_book(id:int,db:Session=Depends(get_db)):
+    delete_entry=services.delete_book(db,id)
+    if delete_entry:
+        return delete_entry
+    raise HTTPException(status_code= 404,detail='Book Not found')
--- services.py
+++ services.py
@@ -11,3 +11,24 @@ def create_book(db:Session,data:BookCreate):
 
 def get_books(db:Session):
     return db.query(Book).all()
+
+def get_book_by_idd(db:Session,book_id:int):
+    return db.query(Book).filter(Book.id==book_id).first()
+
+def update_book(db:Session,book:BookCreate,book_id:int):
+    book_querset=db.query(Book).filter(Book.id==book_id).first()
+    if book_querset:
+        for key,value in book.model_dump().items():
+            setattr(book_querset,key,value)
+        db.commit()
+        db.refresh(book_querset)
+    return book_querset
+
+def delete_book(db:Session,id:int):
+    book_querset=db.query(Book).filter(Book.id==id).first()
+    if book_querset:
+        db.delete(book_querset)
+        db.commit()
+    return book_querset
+
+
